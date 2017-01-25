739dbda55bab13d6d02a9da21b78cc21277d565f
Anas Neumann <anas.neumann.isamm@gmail.com>
2017-01-25 10:15:03 +0100

    Mise en place d'un projet angular

---
+++ SustainappWeb/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sustainappWebApp
+ * @description
+ * # sustainappWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sustainappWebApp', []);
---
+++ SustainappWeb/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sustainappWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sustainappWebApp
+ */
+angular.module('sustainappWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
