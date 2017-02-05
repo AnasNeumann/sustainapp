8c57156097a792d78a270d908a2d3cc633d92602
Anas Neumann <anas.neumann.isamm@gmail.com>
2017-02-05 11:46:53 +0100

    Ionic : Essai récupération dans la gallerie d'image

--- SustainappMobile/www/js/controllers/photoController.js
+++ SustainappMobile/www/js/controllers/photoController.js
@@ -5,14 +5,57 @@
  * @version 1.0
  */
 angular.module('sustainapp.controllers')
-.controller('PhotosCtrl', function($scope, $cordovaCamera) {
-  
+.controller('PhotosCtrl', function($scope, $cordovaCamera, $cordovaFile, $cordovaFileTransfer, $cordovaDevice) {
+
+	
 	/**
 	 * Fonction de prise de photo et affichage
 	 */
-	$scope.takePicture = function() {	
+	$scope.loadPicture = function() {	
 		var options = {
-	      quality: 75,
+	      quality: 100,
+	      destinationType: Camera.DestinationType.DATA_URL,
+	      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+	      allowEdit: true,
+	      encodingType: Camera.EncodingType.JPEG,
+	      targetWidth: 300,
+	      targetHeight: 300,
+	      saveToPhotoAlbum: false
+	    };
+		
+
+		
+	    $cordovaCamera.getPicture(options).then(function(imageData) {
+
+	        window.FilePath.resolveNativePath(imageData, function(entry) {
+	            window.resolveLocalFileSystemURL(entry, success, fail);
+	            function fail(e) {
+	              console.error('Error: ', e);
+	            }
+	     
+	            function success(fileEntry) {
+	              var namePath = fileEntry.nativeURL.substr(0, fileEntry.nativeURL.lastIndexOf('/') + 1);
+	              // Only copy because of access rights
+	              $cordovaFile.copyFile(namePath, fileEntry.name, cordova.file.dataDirectory, newFileName).then(function(success){
+	                $scope.image = newFileName;
+	              }, function(error){
+	                $scope.showAlert('Error', error.exception);
+	              });
+	            };
+	          });
+
+	    	$scope.imgURI = "data:image/jpeg;base64," + imageData;
+	    }, function(err) {
+	      console.log(err);
+	    });
+  };
+
+	/**
+	 * Fonction de prise de photo et affichage
+	 */
+	$scope.takePicture = function(type) {	
+		var options = {
+	      quality: 100,
 	      destinationType: Camera.DestinationType.DATA_URL,
 	      sourceType: Camera.PictureSourceType.CAMERA,
 	      allowEdit: true,
